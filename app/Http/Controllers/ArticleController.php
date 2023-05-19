7413c680f1b8be459b76f677f7662f5723b9e2c6
Alexis Montilla <alexisamm9261@gmail.com>
2023-05-19 11:34:56 -0400

    refactor: local scope created for Article model, this scope has the logic for the filter

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -11,15 +11,7 @@ class ArticleController extends Controller
 {
     public function __invoke(): Renderable
     {
-
-        $status = request()->query("status"); // Estatus viene de la url, como un query param
-        $sort = request()->query("sort"); // Estatus viene de la url, como un query param
-
-        // dd($status, $sort);
-
-        $articles = Article::query()
-            ->when($status, fn(Builder $query) => $query->where("status", $status))
-            ->when($sort, fn(Builder $query) => $query->orderBy("id", $sort))
+        $articles = Article::filtered()
             ->paginate()
             ->withQueryString(); // evita que al cambiar de pagina se pierdan los filtros aplicados por los query params
 
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     use HasFactory;
+
+    public function scopeFiltered(Builder $builder): Builder
+    {
+        return $builder
+            ->when(request()->query("status"), fn(Builder $query) => $query->where("status", request()->query("status"))) // Filtrando por status
+            ->when(request()->query("sort"), fn(Builder $query) => $query->orderBy("id", request()->query("sort"))); // Ordenando por id
+    }
 }
